b9e806b465a2b582af4e9a8a2b07a5478b23e01e
Arron Chapman <unkwntech@gmail.com>
2023-03-21 12:25:23 -0500

    Update main.ts

--- .builder/src/main.ts
+++ .builder/src/main.ts
@@ -1,44 +1,44 @@
-// import * as fs from "fs";
-// import Fit from "./models/fit";
-// function parse(): any {
-//     let file = fs.readFileSync("./test.md").toString().split("\n");
+import * as fs from "fs";
+import Fit from "./models/fit";
+function parse(filename: string): any {
+    let file = fs.readFileSync(filename).toString().split("\n");
 
-//     let buffer = "";
+    let buffer = "";
 
-//     let desc = "";
-//     for (let i = 0; i < file.length; i++) {
-//         if (!file[i].startsWith("##")) continue;
-//         if (file[i].toLowerCase().trim() == "## description") {
-//             i++;
-//             while (true) {
-//                 if (file[i + 1].startsWith("##")) {
-//                     desc = buffer.trim();
-//                     buffer = "";
-//                     break;
-//                 }
-//                 buffer += `${file[i]}`;
-//                 i++;
-//             }
-//         }
+    let desc = "";
+    for (let i = 0; i < file.length; i++) {
+        if (!file[i].startsWith("##")) continue;
+        if (file[i].toLowerCase().trim() == "## description") {
+            i++;
+            while (true) {
+                if (file[i + 1].startsWith("##")) {
+                    desc = buffer.trim();
+                    buffer = "";
+                    break;
+                }
+                buffer += `${file[i]}`;
+                i++;
+            }
+        }
 
-//         if (file[i].toLowerCase().trim() == "## fit") {
-//             while (file[i].trim() !== "```") {
-//                 i++;
-//                 continue;
-//             }
-//             i++;
-//             while (true) {
-//                 if (!file[i + 1] || file[i + 1].startsWith("##")) {
-//                     let fit = Fit.FromEFT(buffer);
-//                     if (desc) fit.description = desc;
-//                     return fit;
-//                 }
-//                 buffer += `${file[i]}`;
-//                 i++;
-//             }
-//         }
-//     }
-// }
+        if (file[i].toLowerCase().trim() == "## fit") {
+            while (file[i].trim() !== "```") {
+                i++;
+                continue;
+            }
+            i++;
+            while (true) {
+                if (!file[i + 1] || file[i + 1].startsWith("##")) {
+                    let fit = Fit.FromEFT(buffer);
+                    if (desc) fit.description = desc;
+                    return fit;
+                }
+                buffer += `${file[i]}`;
+                i++;
+            }
+        }
+    }
+}
 
 // let fit: Fit = parse();
 
@@ -46,9 +46,11 @@
 // console.log(fit.ToXML());
 
 function main(): void {
-console.log(`${process.argv.length} arguments`);
-console.log(JSON.stringify(process.argv.slice(2)));
-
+    let changedFiles = fs.readFileSync(`../${process.argv[2]}`).toString().split("\n");
+    for(let i = 0; i < changedFiles.length; i++) {
+        console.log(`${i} - ${changedFiles[i]}`);
+        console.log(parse(changedFiles[i]).ToXML());
+    }
 }
 
 
